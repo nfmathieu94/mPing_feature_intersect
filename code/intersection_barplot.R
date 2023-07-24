f3529a91eb308a064325964d2dd46e8771facc97
Nathan Mathieu <nmath020@ucr.edu>
2023-07-24 10:38:27 -0700

    Cleaned up some code by removing lines that aren't needed and providing comments

--- code/intersection_barplot.R
+++ code/intersection_barplot.R
@@ -6,13 +6,12 @@ library(wesanderson)
 gff_inter_df <- read.table("../results/gff_intersect_counts.tsv", col.names= c("counts", "feature"))
 intergenic_df <- read.table("../results/intergenic_mPing_intersect.tsv")
 
-
+# Using the number of rows to obtain mPing insertion counts
 intergenic_num <- nrow(intergenic_df)
 
-
+# Adding the total intergenic counts to the list with other features
 gff_inter_df[nrow(gff_inter_df) + 1,] <- list(intergenic_num, "intergenic")
 
-selected_palette <- wes_palette("BottleRocket2")
 
 p <- ggplot(data=gff_inter_df, aes(x=feature, y=counts, fill=feature)) +
     geom_bar(stat="identity")+
